bb976f10df5eb79efaa52088897835858dc7d102
Manu Delmarche <manu.delmarche@gmail.com>
2025-01-19 12:54:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using Iot.Device.DhcpServer;
+using nanoFramework.Networking;
+using nanoFramework.Runtime.Native;
+
+namespace WifiAP
+{
+    public class Program
+    {
+        // Start Simple WebServer
+        private static WebServer _server = new WebServer();
+        private static bool _wifiApMode = false;
+
+        // Connected Station count
+        private static int _connectedCount = 0;
+
+        // GPIO pin used to put device into AP set-up mode
+        private const int SetupPin = 5;
+
+        public static void Main()
+        {
+            Debug.WriteLine("Welcome to WiFI Soft AP world!");
+            Console.WriteLine("Welcome to WiFI Soft AP world!");
+
+            var gpioController = new GpioController();
+            GpioPin setupButton = gpioController.OpenPin(SetupPin, PinMode.InputPullUp);
+
+            // If Wireless station is not enabled then start Soft AP to allow Wireless configuration
+            // or Button pressed
+            if (setupButton.Read() == PinValue.High)
+            {
+                WirelessAP.SetWifiAp();
+                _wifiApMode = true;
+            }
+            else
+            {
+                _wifiApMode = Wireless80211.ConnectOrSetAp();
+            }
+
+            Console.WriteLine($"Connected with wifi credentials. IP Address: {(_wifiApMode ? WirelessAP.GetIP() : Wireless80211.GetCurrentIPAddress())}");           
+            if( _wifiApMode )
+            {
+                _server.Start();
+            }
+
+            // Just wait for now
+            // Here you would have the reset of your program using the client WiFI link
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// Event handler for Stations connecting or Disconnecting
+        /// </summary>
+        /// <param name="NetworkIndex">The index of Network Interface raising event</param>
+        /// <param name="e">Event argument</param>
+        private static void NetworkChange_NetworkAPStationChanged(int NetworkIndex, NetworkAPStationEventArgs e)
+        {
+            Debug.WriteLine($"NetworkAPStationChanged event Index:{NetworkIndex} Connected:{e.IsConnected} Station:{e.StationIndex} ");
+
+            // if connected then get information on the connecting station 
+            if (e.IsConnected)
+            {
+                WirelessAPConfiguration wapconf = WirelessAPConfiguration.GetAllWirelessAPConfigurations()[0];
+                WirelessAPStation station = wapconf.GetConnectedStations(e.StationIndex);
+
+                string macString = BitConverter.ToString(station.MacAddress);
+                Debug.WriteLine($"Station mac {macString} Rssi:{station.Rssi} PhyMode:{station.PhyModes} ");
+
+                _connectedCount++;
+
+                // Start web server when it connects otherwise the bind to network will fail as 
+                // no connected network. Start web server when first station connects 
+                if (_connectedCount == 1)
+                {
+                    // Wait for Station to be fully connected before starting web server
+                    // other you will get a Network error
+                    Thread.Sleep(2000);
+                    _server.Start();
+                }
+            }
+            else
+            {
+                // Station disconnected. When no more station connected then stop web server
+                if (_connectedCount > 0)
+                {
+                    _connectedCount--;
+                    if (_connectedCount == 0)
+                        _server.Stop();
+                }
+            }
+
+        }
+    }
+}
---
+++ WirelessAP.cs
@@ -0,0 +1,160 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using Iot.Device.DhcpServer;
+using nanoFramework.Runtime.Native;
+
+namespace WifiAP
+{
+    /// <summary>
+    /// Provides methods and properties to manage a wireless access point.
+    /// </summary>
+    public static class WirelessAP
+    {
+        /// <summary>
+        /// Gets or sets the IP address of the Soft AP.
+        /// </summary>
+        public static string SoftApIP { get; set; } = "192.168.4.1";
+
+        /// <summary>
+        /// Gets or sets the SSID of the Soft AP.
+        /// </summary>
+        public static string SoftApSsid { get; set; } = "ISCBTargetSystem";
+
+        public static string SoftApPwd { get; set; } = "ISCBShooters";
+
+        /// <summary>
+        /// Sets the configuration for the wireless access point.
+        /// </summary>
+        public static void SetWifiAp()
+        {
+            Wireless80211.Disable();
+            if (Setup() == false)
+            {
+                // Reboot device to Activate Access Point on restart
+                Console.WriteLine($"Setup Soft AP, Rebooting device");
+                Power.RebootDevice();
+            }
+
+            var dhcpserver = new DhcpServer
+            {
+                CaptivePortalUrl = $"http://{SoftApIP}"
+            };
+            var dhcpInitResult = dhcpserver.Start(IPAddress.Parse(SoftApIP), new IPAddress(new byte[] { 255, 255, 255, 0 }));
+            if (!dhcpInitResult)
+            {
+                Console.WriteLine($"Error initializing DHCP server.");
+                // This happens after a very freshly flashed device
+                Power.RebootDevice();
+            }
+
+            Console.WriteLine($"Running Soft AP, waiting for client to connect");
+            Console.WriteLine($"Soft AP IP address :{GetIP()}");
+        }
+
+        /// <summary>
+        /// Disable the Soft AP for next restart.
+        /// </summary>
+        public static void Disable()
+        {
+            WirelessAPConfiguration wapconf = GetConfiguration();
+            wapconf.Options = WirelessAPConfiguration.ConfigurationOptions.None;
+            wapconf.SaveConfiguration();
+        }
+
+        /// <summary>
+        /// Set-up the Wireless AP settings, enable and save
+        /// </summary>
+        /// <returns>True if already set-up</returns>
+        public static bool Setup()
+        {
+            NetworkInterface ni = GetInterface();
+            WirelessAPConfiguration wapconf = GetConfiguration();
+
+            // Check if already Enabled and return true
+            if (wapconf.Options == (WirelessAPConfiguration.ConfigurationOptions.Enable |
+                                    WirelessAPConfiguration.ConfigurationOptions.AutoStart) &&
+                ni.IPv4Address == SoftApIP )
+            {
+                return true;
+            }
+
+            // Set up IP address for Soft AP
+            ni.EnableStaticIPv4(SoftApIP, "255.255.255.0", SoftApIP);
+
+            // Set Options for Network Interface
+            //
+            // Enable    - Enable the Soft AP ( Disable to reduce power )
+            // AutoStart - Start Soft AP when system boots.
+            // HiddenSSID- Hide the SSID
+            //
+            wapconf.Options = WirelessAPConfiguration.ConfigurationOptions.AutoStart |
+                            WirelessAPConfiguration.ConfigurationOptions.Enable;
+
+            // Set the SSID for Access Point. If not set will use default  "nano_xxxxxx"
+            wapconf.Ssid = SoftApSsid;
+
+            // Maximum number of simultaneous connections, reserves memory for connections
+            wapconf.MaxConnections = 1;
+
+            // To set-up Access point with no Authentication
+            //wapconf.Authentication = System.Net.NetworkInformation.AuthenticationType.Open;
+            //wapconf.Password = "";
+
+            // To set up Access point with no Authentication. Password minimum 8 chars.
+            wapconf.Authentication = System.Net.NetworkInformation.AuthenticationType.WPA2;
+            wapconf.Password = SoftApPwd;
+
+            // Save the configuration so on restart Access point will be running.
+            wapconf.SaveConfiguration();
+
+            return false;
+        }
+
+        /// <summary>
+        /// Find the Wireless AP configuration
+        /// </summary>
+        /// <returns>Wireless AP configuration or NUll if not available</returns>
+        public static WirelessAPConfiguration GetConfiguration()
+        {
+            NetworkInterface ni = GetInterface();
+            return WirelessAPConfiguration.GetAllWirelessAPConfigurations()[ni.SpecificConfigId];
+        }
+
+        /// <summary>
+        /// Gets the network interface for the wireless access point.
+        /// </summary>
+        /// <returns>The network interface for the wireless access point.</returns>
+        public static NetworkInterface GetInterface()
+        {
+            NetworkInterface[] Interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            // Find WirelessAP interface
+            foreach (NetworkInterface ni in Interfaces)
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.WirelessAP)
+                {
+                    return ni;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns the IP address of the Soft AP
+        /// </summary>
+        /// <returns>IP address</returns>
+        public static string GetIP()
+        {
+            NetworkInterface ni = GetInterface();
+            return ni.IPv4Address;
+        }
+
+    }
+}
+
